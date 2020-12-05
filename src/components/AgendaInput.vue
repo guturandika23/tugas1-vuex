a1d2365fc4230dbe74c0e8ec70cc5991263e1eaf
guntur andika pramudya <gunturandika2323@gmail.com>
2020-12-05 13:47:58 +0700

    hasil

---
+++ src/components/AgendaInput.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Pesanan</h3>
+            </div>
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="">Menu</label>
+                    <input type="text" v-model="agenda.menu" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="">Harga</label>
+                    <input type="text" v-model="agenda.harga" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-danger btn-sm" :disabled="isLoading" @click.prevent="simpan">{{ isLoading ? 'Loading...':'Tambahkan' }}</button>
+                </div>    
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AgendaInput',
+    data(){
+        return {
+            agenda: {
+                menu: '',
+                harga: ''
+            }
+        }
+    },
+    methods: {
+        simpan(){
+            this.$store.dispatch('simpanAgenda', this.agenda)
+            this.agenda = {
+                menu: '',
+                harga: ''
+            }
+        }
+    },
+    computed: {
+        isLoading(){
+            return this.$store.state.isLoading
+        }
+    }
+}
+</script>
---
+++ src/store.js
@@ -0,0 +1,28 @@
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        isLoading: false,
+        listAgenda: [
+            
+        ]
+    },
+    mutations: {
+        KONFIRMASI_AGENDA: (state, agenda) => {
+            state.listAgenda.push(agenda)
+        }
+    },
+    actions: {
+            simpanAgenda({commit, state} , agenda){
+                state.isLoading = true
+                setTimeout(() => {
+                    commit('KONFIRMASI_AGENDA', agenda)
+                    state.isLoading = false
+                },1500 )
+            }
+    }
+})
